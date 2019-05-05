ddd9f7f16df08da0a2e26b7efc81940bcc0be623
HOME <johnldh1997@gmail.com>
2019-05-05 20:26:43 +1200

    test

--- app/src/main/java/com/example/autprimecampusmap/MainActivity.java
+++ app/src/main/java/com/example/autprimecampusmap/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
