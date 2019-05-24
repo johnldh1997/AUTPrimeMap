693c5bdca2ef531c13e1a0dc746c731e0ac237ac
johnldh1997 <49046672+johnldh1997@users.noreply.github.com>
2019-05-24 14:15:47 +1200

    Update MainActivity.java

--- app/src/main/java/com/example/autprimecampusmap/MainActivity.java
+++ app/src/main/java/com/example/autprimecampusmap/MainActivity.java
@@ -92,7 +92,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         //Adds campus markers to map
         addMarkersToMap();
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && 
+        ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
         {
             return;
         }
